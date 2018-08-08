665a6494f4504dfc9c6c572bc8726ef2f532e3b9
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-08-07 17:35:30 -0700

    Create Bamazon.sql file. Create Products table in Bamazon.sql. Insert mock data into Products table.

---
+++ bamazon.sql
@@ -0,0 +1,31 @@
+/* Create Bamazon database */
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+/* Bamazon product table */
+CREATE TABLE Products(
+    ItemID INT(10) AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    DepartmentName VARCHAR(100) NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    StockQuantity INT(10) NOT NULL,
+    primary key(ItemID)
+);
+
+/* Select (all) from Products */
+select * from Products;
+
+/* Insert mock products to table */
+INSERT INTO Products(ProductName, DepartmentName, Price, StockQuantity)
+Values
+("playstation 4", "technology", 249.99, 100),
+("macbook pro", "technology", 1999.99, 50),
+("galaxy s7", "technology", 499.99, 50),
+("venus fly trap", "gardening", 7.99, 30),
+("potting soil", "gardening", 14.99, 20),
+("basket ball", "sports", 19.99, 40),
+("hand wraps". "sports", 10.99, 35),
+("dog food", "pets", 19.99, 40),
+("cat litter", "pets", 15.99, 20),
+("slippers", "clothing", 19.99, 25);
